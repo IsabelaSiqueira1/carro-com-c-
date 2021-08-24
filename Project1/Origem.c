1bbe6556fc902e9b5bd0a14110fd82720c1a807a
isabela siqueira <isabela.generoso14@gmail.com>
2021-08-24 20:53:40 -0300

    Carro com c++

---
+++ Project1/Origem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("\n\n");
+	printf("\n\t\xDC\xDC\xDB\xDB\xDB\xDB\xDC\xDC");
+	printf("\n\t\xDFO\xDF\xDF\xDF\xDFO\xDF");
+	printf("\n\n");
+
+	return 0;
+
+}
